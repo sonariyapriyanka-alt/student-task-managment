e1e70b3230e530fe1f6de694b884ab0dc8110e4b
priyanka <sonariyapriyanka@gmail.com>
2026-01-30 13:45:58 +0530

    [PS] [project added]

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/header'
+import Register from './pages/Register'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Headerp/>
+      <Header/>
+      <Register/>
     </>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <>
+    <div className="form-container">
+        <h1>This is Register</h1>
+
+    </div> 
+    </>
+  );
+};
+
+export default Register;
